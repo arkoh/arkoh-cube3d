e7753c6c1a2b1b1a345c74b0f0938ed23c963416
arkoh <arkoh0777@gmail.com>
2014-01-19 01:58:54 +0900

    test Open-gl API

--- src/main/window.rs
+++ src/main/window.rs
@@ -1,4 +1,5 @@
 use glfw;
+use gl;
 use std::libc;
 
 
@@ -14,9 +15,16 @@ impl<'a> Window<'a> {
         do glfw::start {
             let window = glfw::Window::create(300, 300, "Hello this is window", glfw::Windowed)
                          .expect("Failed to create GLFW window.");
-
             window.set_key_callback(~KeyContext);
             window.make_context_current();
+
+            gl::load_with(glfw::get_proc_address);
+
+            gl::FrontFace(gl::CCW);
+            gl::Enable(gl::DEPTH_TEST);
+            gl::Enable(gl::SCISSOR_TEST);
+            gl::DepthFunc(gl::LEQUAL);
+
 
             let mut user_window = Window { window: window };
             user_callback(&mut user_window);
@@ -25,8 +33,16 @@ impl<'a> Window<'a> {
 
     pub fn render_loop(&mut self, callback: fn(&mut Window) ) {
         while !self.window.should_close() {
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::ClearColor(0.0,1.0,1.0,1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::Clear(gl::DEPTH_BUFFER_BIT);
+           
+            gl::Scissor(0 as i32, 0 as i32, 300 as i32, 300 as i32);
+
             glfw::poll_events();
             callback(self);
+            self.window.swap_buffers();
         }
     }
 }
